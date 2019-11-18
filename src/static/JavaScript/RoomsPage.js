33e2b1c3983354ce8c1299622d0e835dc5c1bfcc
Marta Ribeiro <1191779@isep.ipp.pt>
2019-11-18 11:33:54 +0000

    Created JavaScript file. Created onclick e onload functions.

---
+++ src/static/JavaScript/RoomsPage.js
@@ -0,0 +1,23 @@
+
+function reloadMessage() {
+    var request = new XMLHttpRequest();
+    request.onload = function upDate() {
+        document.getElementById("outputBox").innerHTML = this.responseText;
+        setTimeout(reloadMessage, 1500);
+    };
+    request.ontimeout = function timeoutCase() {
+        document.getElementById("outputBox").innerHTML = "Still trying ...";
+        setTimeout(reloadMessage, 1000);
+    };
+    request.onerror = function errorCase() {
+        document.getElementById("outputBox").innerHTML = "Still trying ...";
+        setTimeout(reloadMessage, 1000);
+    };
+    request.open("GET", "https://vs280.dei.isep.ipp.pt/cgi-bin/outputBox", true);
+    request.timeout = 5000;
+    request.send();
+}
+
+function sendMessage () {
+
+}
