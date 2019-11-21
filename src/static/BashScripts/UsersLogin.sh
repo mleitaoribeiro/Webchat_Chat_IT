0cc7e3dcd2c3f9115f2691b22dfd6a30655e78a8
Marta Ribeiro <1191779@isep.ipp.pt>
2019-11-21 16:02:30 +0000

    Corrected UsersLogin. Corrected WelcomePage.js.

--- src/static/BashScripts/UsersLogin.sh
+++ src/static/BashScripts/UsersLogin.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 USERS_REPOSITORY=/var/www/cgi-bin/usersLogin.log
-if [ "$REQUEST_METHOD" == "POST" ]; then
+if [ "$REQUEST_METHOD" == "PUT" ]; then
   echo "Content-type: text/plain"
   echo "Access-Control-Allow-Origin: *"
   echo ""
   if [ ! -f $USERS_REPOSITORY ]; then
     touch $USERS_REPOSITORY && chmod a+w+x $USERS_REPOSITORY
   fi
-  IS_IN_REPOSITORY=$(cat ${USERS_REPOSITORY}|grep -c "($M_CONTENT_FILE)")
+  IS_IN_REPOSITORY=$(cat ${USERS_REPOSITORY}|grep -c "${M_CONTENT_FILE}")
   echo $IS_IN_REPOSITORY
   COUNT_USERS_ONLINE=$(cat $USERS_REPOSITORY|wc -l)
   if [ $IS_IN_REPOSITORY -eq 0 ]; then
-    echo "$M_CONTENT_FILE" >> $USERS_REPOSITORY
+    cat $M_CONTENT_FILE >> $USERS_REPOSITORY
+    echo "" >> $USERS_REPOSITORY
+    echo "200"
   fi
   if [ $IS_IN_REPOSITORY -gt 0 ]; then
     echo "<p><i class='fas fa-exclamation-circle'></i> This nickname already exists. Please enter a new one!</p>"
@@ -22,4 +24,3 @@ if [ "$REQUEST_METHOD" == "POST" ]; then
   exit
 fi
 echo "Status: 400 Bas Request"
-
