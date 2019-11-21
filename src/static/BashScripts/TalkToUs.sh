fd3f9b64ead1a98dd5356f92f8dcbffe24e7b803
alex <1191743@isep.ipp.pt>
2019-11-21 13:59:11 +0000

    Script Bash update talk to Us

---
+++ src/static/BashScripts/TalkToUs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+VAR_EMAIL=/var/www/cgi-bin/emailContent
+to="1191743@isep.ipp.pt"
+if [ "$REQUEST_METHOD" == "POST" ]; then
+  echo "Content-type: text/plain"
+  echo "Access-Control-Allow-Origin: *"
+  echo ""
+  if [ ! -f $VAR_EMAIL ]; then echo "" >> $VAR_EMAIL && chmod a+w $VAR_EMAIL; fi
+  echo "$(date) $(cat $M_CONTENT_FILE)" > $VAR_EMAIL
+  sendmail $to < $VAR_EMAIL
+exit
+fi
+echo "Status: 400 Bad Request"
