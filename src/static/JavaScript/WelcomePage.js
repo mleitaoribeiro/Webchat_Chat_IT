f121f0e67013bbd96034770efe7bbb39ceea99dc
Marta Ribeiro <1191779@isep.ipp.pt>
2019-11-21 10:57:04 +0000

    Corrected OnlineUsers, UsersLogin. Corrected WelcomePage.js e WelcomePage.

--- src/static/JavaScript/WelcomePage.js
+++ src/static/JavaScript/WelcomePage.js
@@ -4,10 +4,10 @@ function loginUser() {
     console.log(nickname);
     var request = new XMLHttpRequest();
     request.onload = function() {
-        if (this.readyState == 4 && this.status != 200) {
+        if (this.readyState == 4 && this.status == 200) {
             document.getElementById("errorMessage").innerHTML = this.response;
         }
     };
-    request.open("PUT", "http://vs280.dei.isep.ipp.pt/cgi-bin/userLogin", true);
+    request.open("POST", "http://vs280.dei.isep.ipp.pt/cgi-bin/usersLogin", true);
     request.send(nickname);
 }
