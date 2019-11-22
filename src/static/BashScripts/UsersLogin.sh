e98231f37a428a8abf3d2df41ef3307537995b1c
Marta Ribeiro <1191779@isep.ipp.pt>
2019-11-22 09:38:04 +0000

    Corrected UsersLogin.

--- src/static/BashScripts/UsersLogin.sh
+++ src/static/BashScripts/UsersLogin.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 USERS_REPOSITORY=/var/www/cgi-bin/usersLogin.log
 if [ "$REQUEST_METHOD" == "PUT" ]; then
-  echo "Content-type: text/plain"
-  echo "Access-Control-Allow-Origin: *"
-  echo ""
-  if [ ! -f $USERS_REPOSITORY ]; then
-    touch $USERS_REPOSITORY && chmod a+w+x $USERS_REPOSITORY
-  fi
-  IS_IN_REPOSITORY=$(cat ${USERS_REPOSITORY}|grep -c "${M_CONTENT_FILE}")
-  echo $IS_IN_REPOSITORY
-  COUNT_USERS_ONLINE=$(cat $USERS_REPOSITORY|wc -l)
-  if [ $IS_IN_REPOSITORY -eq 0 ]; then
-    cat $M_CONTENT_FILE >> $USERS_REPOSITORY
-    echo "" >> $USERS_REPOSITORY
-    echo "200"
-  fi
-  if [ $IS_IN_REPOSITORY -gt 0 ]; then
-    echo "<p><i class='fas fa-exclamation-circle'></i> This nickname already exists. Please enter a new one!</p>"
-  fi
-  if [ $COUNT_USERS_ONLINE -eq 50 ]; then
-    echo "<p> This chat is temporarily unavailable due to excessive load.</p>"
-  fi
-  exit
+ echo "Content-type: text/plain"
+ echo "Access-Control-Allow-Origin: *"
+ echo ""
+
+ if [ ! -f $USERS_REPOSITORY ]; then
+  touch $USERS_REPOSITORY && chmod a+w+x $USERS_REPOSITORY
+ fi
+
+ IS_IN_REPOSITORY=$(grep -c ${M_CONTENT_FILE} $USERS_REPOSITORY)
+ echo $IS_IN_REPOSITORY
+ COUNT_USERS_ONLINE=$(cat $USERS_REPOSITORY|wc -l)
+ if [ $COUNT_USERS_ONLINE -eq 50 ]; then
+  echo "<p> This chat is temporarily unvailable due to excessive load.</p>"
+ fi
+ if [ $IS_IN_REPOSITORY -eq 0 ]; then
+   cat $M_CONTENT_FILE
+   echo $M_CONTENT_FILE >> $USERS_REPOSITORY
+   echo "" >> $USERS_REPOSITORY
+   echo "True"
+ fi
+ if [ $IS_IN_REPOSITORY -gt 0 ]; then
+  echo "<p><i class='fas fa-exclamation-circle'></i> This nickname already exists. Please enter a new one <p>"
+ fi
+ exit
 fi
-echo "Status: 400 Bas Request"
+echo "Status: 400 Bad Request"
