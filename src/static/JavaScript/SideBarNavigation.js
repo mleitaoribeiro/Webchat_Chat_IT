7d80fcd182ece1f387388184b0e6a2fc094ebe59
marta <1191780@isep.ipp.pt>
2019-12-02 18:26:49 +0000

    #javascript: destacar sala que está a ser utilizada - teste !! NAO ESTRAGA NADA

---
+++ src/static/JavaScript/SideBarNavigation.js
@@ -0,0 +1,24 @@
+// WTF
+
+function sideNav() {
+    // Get the container element
+    var btnContainer = document.getElementById("User_Available_Rooms");
+
+// Get all buttons with class="btn" inside the container
+    var btns = btnContainer.getElementsByClassName("chatrooms");
+
+// Loop through the buttons and add the active class to the current/clicked button
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function() {
+            var current = document.getElementsByClassName(" active");
+
+            // If there's no active class
+            if (current.length > 0) {
+                current[0].className = current[0].className.replace(" active", "");
+            }
+
+            // Add the active class to the current/clicked button
+            this.className += " active";
+        });
+    }
+}
