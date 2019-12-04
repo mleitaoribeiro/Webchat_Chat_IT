db643067efaea29a221f6b1086a4f29305c4b842
Marta Ribeiro <1191779@isep.ipp.pt>
2019-12-04 16:22:22 +0000

    Correção da função loginUser

--- src/static/JavaScript/WelcomePage.js
+++ src/static/JavaScript/WelcomePage.js
@@ -1,30 +1,28 @@
 // Função que envia o nickname introduzido no Login e o envia para validação no servidor:
 function loginUser() {
     var nickname = document.getElementById("nickname").value;
-    if(nickname.length > 1) {
-        localStorage.setItem( 'nickname', nickname );
-        var request = new XMLHttpRequest();
-        request.onreadystatechange = function login () {
-            if (this.readyState === 4 && this.status === 200) {
-                window.location.href = "ChatRoomPage.html"; ///NAO ESQUECER QUE TEM SE MUDAR!!!!!!!!!!!
-            }
-            else {
-                document.getElementById("errorMessage").innerHTML = this.response;
-            }
-            setTimeout(reloadRoom, 500);
-        };
-        request.ontimeout = function timeoutCase() {
-            document.getElementById("errorMessage").innerHTML = "Still trying ...";
-            setTimeout(reloadRoom, 500);
-        };
-        request.onerror = function onError () {
-            document.getElementById("errorMessage").innerHTML = "Still trying ...";
-            setTimeout(reloadRoom, 500);
-        };
-        request.open ("PUT" , "https://vs-gate.dei.isep.ipp.pt:26280/cgi-bin/usersLogin", true); //MUDAR QUANDO FOR PARA O SERVIDOR
-        request.timeout = 5000;
-        request.send(nickname);
-    }
+    localStorage.setItem( 'nickname', nickname );
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function login () {
+        if (this.readyState === 4 && this.status === 200) {
+            window.location.href = "ChatRoomPage.html"; ///NAO ESQUECER QUE TEM SE MUDAR!!!!!!!!!!!
+        }
+        else {
+            document.getElementById("errorMessage").innerHTML = this.response;
+        }
+        setTimeout(reloadRoom, 500);
+    };
+    request.ontimeout = function timeoutCase() {
+        document.getElementById("errorMessage").innerHTML = "Still trying ...";
+        setTimeout(reloadRoom, 500);
+    };
+    request.onerror = function onError () {
+        document.getElementById("errorMessage").innerHTML = "Still trying ...";
+        setTimeout(reloadRoom, 500);
+    };
+    request.open ("PUT" , "https://vs-gate.dei.isep.ipp.pt:26280/cgi-bin/usersLogin", true); //MUDAR QUANDO FOR PARA O SERVIDOR
+    request.timeout = 5000;
+    request.send(nickname);
 }
 
 // Função que impede determinados caractéres no campo de input para o nickname:
